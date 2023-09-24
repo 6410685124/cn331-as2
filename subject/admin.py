56eb2d156468c3790bb404fe6da16557409658d7
Tanakrit Iewwangso <6410685124@cn103>
2023-09-24 09:11:10 +0700

    finish admin section

--- subject/admin.py
+++ subject/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Class, Subject, Student
+
+class classAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year', 'semester', 'total_seats', 'remaining_seats')
+    filter_horizontal = ('students',)
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'student_id')
+
+admin.site.register(Class, classAdmin)
+admin.site.register(Subject)
+admin.site.register(Student, StudentAdmin)
--- subject/models.py
+++ subject/models.py
@@ -1,3 +1,55 @@
+from collections.abc import Iterable
 from django.db import models
 
 # Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    code = models.CharField(max_length=10, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    student_id = models.CharField(max_length=10, unique=True)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Class(models.Model):
+    YEAR_CHOICES = (
+        ('1st Year', '1st Year'),
+        ('2nd Year', '2nd Year'),
+        ('3rd Year', '3rd Year'),
+        ('4th Year', '4th Year'),
+    )
+
+    SEMESTER_CHOICES = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+    )
+
+    name = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="subject_name")
+    year = models.CharField(max_length=10, choices=YEAR_CHOICES)
+    semester = models.CharField(max_length=10, choices=SEMESTER_CHOICES)
+    students = models.ManyToManyField(Student, related_name='classes')
+    # total_seats = models.PositiveIntegerField(default=0)
+    # remaining_seats = models.PositiveIntegerField(default=99)
+    max_seats = models.PositiveIntegerField(default=99)
+
+    def __str__(self):
+        return f"{self.year} - {self.semester} - {self.name}"
+    
+    @property
+    def total_seats(self):
+        return self.students.count()
+    
+    @property
+    def remaining_seats(self):
+        max_seat = self.max_seats
+        total_seat = self.total_seats
+        return max_seat - total_seat
+    
