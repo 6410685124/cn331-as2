2cc09b0cc4c5d69f7521e2818d0e036a7690eff0
Tanakrit Iewwangso <6410685124@cn103>
2023-10-07 10:52:51 +0700

    change model

--- subject/models.py
+++ subject/models.py
@@ -4,21 +4,18 @@ from django.db import models
 # Create your models here.
 
 class Subject(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+
     code = models.CharField(max_length=10, unique=True)
+    name = models.CharField(max_length=100, unique=False)
 
     def __str__(self):
-        return self.name
+        return self.code
 
 class Student(models.Model):
+
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     student_id = models.CharField(max_length=10, unique=True)
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
-
-class Class(models.Model):
     YEAR_CHOICES = (
         ('1st Year', '1st Year'),
         ('2nd Year', '2nd Year'),
@@ -26,22 +23,29 @@ class Class(models.Model):
         ('4th Year', '4th Year'),
     )
 
+    year = models.CharField(max_length=10, default=None, choices=YEAR_CHOICES)
+
+    def __str__(self):
+        return f"{self.student_id} {self.first_name} {self.last_name}"
+
+
+class Class(models.Model):
+
+    name = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="subject_name", to_field='code')
+    students = models.ManyToManyField(Student, related_name='classes')
+    max_seats = models.PositiveIntegerField(default=99)
+    
     SEMESTER_CHOICES = (
         ('1', '1'),
         ('2', '2'),
         ('3', '3'),
     )
-
-    name = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name="subject_name")
-    year = models.CharField(max_length=10, choices=YEAR_CHOICES)
-    semester = models.CharField(max_length=10, choices=SEMESTER_CHOICES)
-    students = models.ManyToManyField(Student, related_name='classes')
-    max_seats = models.PositiveIntegerField(default=99)
-    status = models.BooleanField(default=True)
+    semester = models.CharField(max_length=10, default=None, choices=SEMESTER_CHOICES)
 
     def __str__(self):
-        return f"{self.year} - {self.semester} - {self.name}"
+        return f"{self.name}"
     
+    status = models.BooleanField(default=True)
     @property
     def total_seats(self):
         return self.students.count()
@@ -51,4 +55,10 @@ class Class(models.Model):
         max_seat = self.max_seats
         total_seat = self.total_seats
         return max_seat - total_seat
+    
+    def add_student(self, student):
+        self.students.add(student)
+
+    def remove_student(self, student):
+        self.students.remove(student)
     
--- subject/views.py
+++ subject/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+# def index(request):
+#     if not request.user.is_authenticated:
+#         return render(request, "student/login.html")
+#     return render(request, 'subject/index.html')
